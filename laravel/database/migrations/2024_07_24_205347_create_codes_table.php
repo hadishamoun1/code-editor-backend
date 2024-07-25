2d67306046d939d4e6987eb692850c312f815e9d
hadishamoun1 <shamounhadi2@gmail.com>
2024-07-25 03:18:31 +0300

    added migration tables

---
+++ laravel/database/migrations/2024_07_24_205347_create_codes_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('codes', function (Blueprint $table) {
+            $table->id(); // primary key
+            $table->foreignId('user_id')->constrained('users'); // foreign key for table users
+            $table->string('title',255); // varchar(255)
+            $table->text('content'); // text
+            $table->string('language', 50); // varchar(50)
+            $table->timestamps(); // includes created_at and updated_at
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('codes');
+    }
+};
