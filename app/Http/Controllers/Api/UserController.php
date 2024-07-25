cfefe800077e5a154f9d203e0613f297b0e719b8
hadishamoun1 <shamounhadi2@gmail.com>
2024-07-25 23:35:59 +0300

    arranged folders and added user api

---
+++ app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    // Display a listing of the resource.
+    public function index()
+    {
+        return User::all();
+    }
+
+    // Display the specified resource.
+    public function show($id)
+    {
+        return User::findOrFail($id);
+    }
+
+    // Store a newly created resource in storage.
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255',
+            'password' => 'required|string|min:8',
+        ]);
+
+        return User::create($validated);
+    }
+
+    // Update the specified resource in storage.
+    public function update(Request $request, $id)
+    {
+        $user = User::findOrFail($id);
+
+        $validated = $request->validate([
+            'name' => 'sometimes|string|max:255',
+             'email' => 'sometimes|string|email|max:255' ,
+             'password' => 'sometimes|string|min:8',
+        ]);
+
+        $user->update($validated);
+
+        return $user;
+    }
+
+    // Remove the specified resource from storage.
+    public function destroy($id)
+    {
+        $post = User::findOrFail($id);
+        $post->delete();
+
+        return response()->noContent();
+    }
+}
